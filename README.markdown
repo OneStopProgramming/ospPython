981639bbca22143744f244eb0aac248f4c77be3e
OneStopProgramming <info@OneStopProgramming.com>
2014-06-29 17:06:04 -0400

    Initial Commit

---
+++ README.markdown
@@ -0,0 +1,27 @@
+ospPython
+=============
+
+ospPython is a no hassle work around to everyone who is having issues with
+setting up the environmental variables for python. Simply copy the ospPython.exe
+and ospPython.exe.config files from the "ospPython Release" folder to your 
+"C:\Windows\System32" folder. Start a new command prompt and type ospPython 
+to start a python shell. Python scripts can be run using ospPython [pathTo.py].
+
+Instillation
+------------
+* Download the repository
+* Copy both ospPython.exe and ospPython.exe.config from the "ospPython Release" folder to your 
+"C:\Windows\System32" folder.
+* Change the "pythonPath" attribute in the ospPython.exe.config to point to your python.exe.
+* Use the ospPython command from the command line
+
+Licence
+------------
+GNU General Public License, version 3 (GPL-3.0)
+
+The user assumes all responsibilities for how this program, its source files, related files, and configuration
+files are used. By using this software the user agrees to assume all responsibility for any liabilities, 
+damages, or misuse of this software. The author of this software is not responsible for how this software is used
+or for any results occurring from running this software. Software and program, as used used in this agreement, is defined as
+any source file, configuration file, binary file, or other related file/resource that is included in this repository or 
+created as a result of downloading or using this repository. 
---
+++ ospPython/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace ospPython
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = ConfigurationManager.AppSettings["pythonPath"];
+            string commandArg;
+
+            if (command == "")
+                return;
+
+            //Verify exe exists
+            if (!File.Exists(command))
+            {
+                Console.WriteLine("Error: The python.exe was not found.");
+                Console.WriteLine("You must specify the path to the python.exe in the ospPython.exe.config file");
+                Console.WriteLine("Open the ospPython.exe.config file with a text editior and specify the path to python.exe");
+                Console.WriteLine("Example:C:\\Python33\\python.exe");
+                Console.WriteLine();
+                Console.WriteLine("Verify that ospPython.exe.config exists");
+                Console.WriteLine("This .config file must be located in the same folder as ospPython.exe");
+                Console.WriteLine("ospPython.exe.config can be downloaded from https://github.com/OneStopProgramming/ospPython.");
+                
+                return;
+            }
+            if (args.Length != 1)
+            {
+                commandArg = "";
+
+            }
+            else
+            {
+                commandArg = args[0];
+            }
+
+            Console.WriteLine(command + " " + commandArg);
+
+            Process process = new Process();
+            process.StartInfo.FileName = command;
+            process.StartInfo.Arguments = commandArg;
+            try
+            {
+                process.Start();
+            }
+            catch
+            {
+                Console.WriteLine("Error: An unknown error has occured in ospPython.");
+                Console.WriteLine("    Ussage: ospPython path\\to\\yourPythonScript.py");
+                Console.WriteLine("    Example: ospPython HelloWorld.py");
+                Environment.Exit(1);
+            }
+            return;
+        }
+    }
+}
